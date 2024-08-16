87f95f4f4c775674a03eb5eb211cab8dcaae5542
keeganmhawkins@gmail.com <keeganmhawkins@gmail.com>
2024-08-16 07:50:35 -0500

    Finishing touches

--- github-search/src/components/Profiles.jsx
+++ github-search/src/components/Profiles.jsx
@@ -1,8 +1,42 @@
 const Profiles = ({ profile }) => {
+  console.log(profile);
+
   return (
     <div className="profiles-container">
-      <h1>Profile</h1>
-      <p>{profile.login}</p>
+      <div className="profile-card">
+        <div className="card-header">
+          <h1>{profile.login}</h1>
+          <img className="pfp" src={profile.avatar_url} alt="Profile avatar" />
+        </div>
+        <div className="card-body">
+          <div className="card-bio">
+            <h3>{profile.name}</h3>
+            <p>{profile.bio}</p>
+          </div>
+          <div className="card-stats">
+            <p className="stat">Followers: {profile.followers}</p>
+            <p className="stat">Repos: {profile.public_repos}</p>
+            <p className="stat">Gists: {profile.public_gists}</p>
+          </div>
+        </div>
+        <div className="card-footer">
+          <h2>Links</h2>
+          <ul className="links">
+            <li className="link">
+              <a href={profile.url}>Profile Page</a>
+            </li>
+            <li className="link">
+              <a href={profile.repos_url}>Repos</a>
+            </li>
+            <li className="link">
+              <a href={profile.subscriptions_url}>Subscriptions</a>
+            </li>
+            <li className="link">
+              <a href={profile.organizations_url}>Organizations</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
